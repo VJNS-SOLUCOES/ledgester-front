906e419fd384e8d13bc65f87aadd45176e1c3aba
SamuSNascimento <samuelsantosn4@gmail.com>
2024-05-30 10:06:23 -0300

    fix: index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Wrapper } from '../components';
-import { Bar } from 'react-chartjs-2';
+// import { Bar } from 'react-chartjs-2';
 import 'chart.js/auto';
 
 const MainPage: React.FC = () => {
