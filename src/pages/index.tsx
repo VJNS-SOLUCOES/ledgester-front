ecb01f118402ee30ae14ec7a1479b55ed2a68dfc
SamuSNascimento <samuelsantosn4@gmail.com>
2024-05-30 09:25:47 -0300

    feat: improvments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Wrapper } from '../components';
+import { Bar } from 'react-chartjs-2';
+import 'chart.js/auto';
 
 const MainPage: React.FC = () => {
   return (
     <Wrapper>
-      <h1>ola</h1>
+      <div className="bg-black h-64">
+        {/*<Bar
+          className="bg-white"
+          data={{
+            labels: ['A', 'B', 'C'],
+            datasets: [{ label: 'ola', data: [200, 300, 200] }],
+          }}
+        />
+        */}
+      </div>
     </Wrapper>
   );
 };
